d67b85b3b7b6619bebbc76d9fca4c73113413229
marcospablo-br <mpablo2012@gmail.com>
2022-05-04 08:57:53 -0300

    Delete folder Helpers

--- MeuVelho.Infra/Data/Contexts/MeuVelhoContext.cs
+++ MeuVelho.Infra/Data/Contexts/MeuVelhoContext.cs
@@ -1,6 +1,8 @@
 ﻿using MeuVelho.Domains;
 using MeuVelho.Infra.Data.Mappings;
 using Microsoft.EntityFrameworkCore;
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+#pragma warning disable CS8618
 
 namespace MeuVelho.Infra.Data.Contexts
 {
@@ -23,9 +25,7 @@ namespace MeuVelho.Infra.Data.Contexts
             modelBuilder.ApplyConfiguration(new CityMap());
             modelBuilder.ApplyConfiguration(new ConnectionMap());
         }
-
-        public DbSet<CountryDomain> Countries { get; set; }
-        public DbSet<StateDomain> States { get; set; }
+        
         public DbSet<CityDomain> Cities { get; set; }
         public DbSet<CaregiverDomain> Caregivers { get; set; }
         public DbSet<ConnectionDomain> Connections { get; set; }
